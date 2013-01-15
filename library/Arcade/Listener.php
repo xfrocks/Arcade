15786d77a38f48b335ec810f9c54c6b55a8919bf
Kevin <kevin.wilkinson@gmail.com>
2013-01-15 12:02:50 -0500

    Added "Enable NavBar Link" Option

--- library/Arcade/Listener.php
+++ library/Arcade/Listener.php
@@ -2,26 +2,22 @@
 class Arcade_Listener {
 	public static function navigation_tabs(array &$extraTabs,$seletectedTabId) {
 		$visitor = XenForo_Visitor::getInstance();
-
-		if (XenForo_Model::create('Arcade_Model_Game')->hasPermission('navbar')) {
-			
-			
-			$options = XenForo_Application::get('options');
-		
-			$tabId = 'arcade';
-			
-			/// @todo: implement arcade_links template, need way of linking directly to a category tab
-			
-			$extraTabs[$tabId] = array(
-				'href' => XenForo_Link::buildPublicLink("full:arcade"),
-				'title' => new XenForo_Phrase('arcade'),
-				//'linksTemplate' => 'arcade_links',
-				'selected' => ($seletectedTabId == $tabId),
-			);
-			
-			if(!$options->xfarcade_enable_categories)
-				unset($extraTabs[$tabId]['linksTemplate']);
-		}
+	 	$autoNavbar = XenForo_Application::get('options')->xfarcade_auto_navbar;
+		if ($autoNavbar) {
+     		if (XenForo_Model::create('Arcade_Model_Game')->hasPermission('navbar')) {
+     	    		$options = XenForo_Application::get('options');
+	         		$tabId = 'arcade';
+     			/// @todo: implement arcade_links template, need way of linking directly to a category tab
+     			$extraTabs[$tabId] = array(
+     				'href' => XenForo_Link::buildPublicLink("full:arcade"),
+     				'title' => new XenForo_Phrase('arcade'),
+     				//'linksTemplate' => 'arcade_links',
+     				'selected' => ($seletectedTabId == $tabId),
+     			);
+     			if(!$options->xfarcade_enable_categories)
+     				unset($extraTabs[$tabId]['linksTemplate']);
+               }
+          }
 	}
 	
 	public static function load_class($class, array &$extend) {
