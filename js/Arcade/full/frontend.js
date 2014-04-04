4b7f8c96645e0d69f2e521c1e40629c8610d953e
Dao Hoang Son <daohoangson@gmail.com>
2014-04-04 14:21:25 +0700

    fixed js error

---
+++ js/Arcade/full/frontend.js
@@ -0,0 +1,42 @@
+(function($)
+{
+	XenForo.GameVoteLink = function($link)
+	{
+		var $target = $('.game-vote-' + $link.data('gameid'));
+
+		$link.click(function(e)
+		{
+			e.preventDefault();
+			var $link = $(this);
+
+			if ($link.hasClass('GameVoted'))
+			{
+				XenForo.alert($target.data('message'));
+				return;
+			}
+
+			XenForo.ajax(this.href,
+			{
+			}, function(ajaxData, textStatus)
+			{
+				if (XenForo.hasResponseError(ajaxData))
+				{
+					return false;
+				}
+
+				$link.stop(true, true);
+
+				if (ajaxData.templateHtml === '')
+				{
+					$target.xfRemove();
+				}
+				else
+				{
+					$target.html($(ajaxData.templateHtml).html()).xfActivate();
+				}
+			});
+		});
+	};
+
+	XenForo.register('a.GameVoteLink', 'XenForo.GameVoteLink');
+})(jQuery); 
